32f48a9377e66af4d66a01723438c1d1285f9935
Alagie Singhateh <3939919@gmail.com>
2025-04-28 02:03:53 +0100

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,7 @@ import React from "react";
 import router from "./Router"; // Custom Router logic
 import { ContextProvider } from "./contex/ContexProvider";
 import { RouterProvider } from "react-router-dom"; // Import HashRouter
-import { registerSW } from 'virtual:pwa-register';
 
-registerSW({
-  onNeedRefresh() {
-    console.log("New content available, refresh the page.");
-  },
-  onOfflineReady() {
-    console.log("App is ready to work offline.");
-  },
-});
 
 function App() {
 
