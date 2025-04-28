34af34dc499d37bdaa35103aff3f8c811c59ce2a
Alagie Singhateh <3939919@gmail.com>
2025-04-28 01:45:42 +0100

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -7,37 +7,37 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'My React Vite App',
-        short_name: 'ReactApp',
-        description: 'My awesome Vite-powered PWA!',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/zax-react-frontend/',  // Change this line to match the subdirectory
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+    //   manifest: {
+    //     name: 'My React Vite App',
+    //     short_name: 'ReactApp',
+    //     description: 'My awesome Vite-powered PWA!',
+    //     theme_color: '#ffffff',
+    //     background_color: '#ffffff',
+    //     display: 'standalone',
+    //     start_url: '/zax-react-frontend/',  // Change this line to match the subdirectory
+    //     icons: [
+    //       {
+    //         src: '/pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: '/pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: '/pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any maskable'
+    //       }
+    //     ]
+    //   }
+    // })
   ],
-  base: '/zax-react-frontend/',  // Make sure this matches your GitHub Pages subdirectory only not local dev
+  // base: '/zax-react-frontend/',  // Make sure this matches your GitHub Pages subdirectory only not local dev
 })
